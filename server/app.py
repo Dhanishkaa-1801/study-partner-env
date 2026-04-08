b38ae5db5d5acce5bc05e09df6d603c5b8315834
Azy1801 <dhanishkaabiz@gmail.com>
2026-04-08 22:09:06 +0530

    fix errors

--- server/app.py
+++ server/app.py
@@ -124,7 +124,7 @@ def state():
 # ─────────────────────────────────────────────
 
 def main():
-    uvicorn.run("server.app:app", host="0.0.0.0", port=8000)
+    uvicorn.run("server.app:app", host="0.0.0.0", port=7680)
 
 if __name__ == "__main__":
     main()
